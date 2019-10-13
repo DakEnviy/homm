466d8c99000b16518beebd984f7ff5adc2b31901
DakEnviy <dakenviy@gmail.com>
2019-10-13 20:44:54 +0300

    Added some Units

---
+++ HOMM/Units/UnitAngel.cs
@@ -0,0 +1,8 @@
+namespace HOMM.Units
+{
+    public class UnitAngel : Unit
+    {
+        public UnitAngel()
+            : base(UnitType.Angel, 180, 27, 27, (45, 45), 11.0f) {}
+    }
+}
---
+++ HOMM/Units/UnitSkeleton.cs
@@ -0,0 +1,8 @@
+namespace HOMM.Units
+{
+    public class UnitSkeleton : Unit
+    {
+        public UnitSkeleton()
+            : base(UnitType.Skeleton, 5, 1, 2, (1, 1), 10.0f) {}
+    }
+}
