224dbd5cfbef016d9217f1965db9c4927da34eb0
DakEnviy <dakenviy@gmail.com>
2019-10-13 22:27:24 +0300

    Add exceptions to UnitsStack and Army

--- HOMM/Army.cs
+++ HOMM/Army.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections.Generic;
 
 namespace HOMM
 {
     public class Army
     {
+        public static readonly uint MAX_ARMY_SIZE = 6;
+        
         private readonly List<UnitsStack> _stacks;
         
         public Army(List<UnitsStack> stacks)
         {
+            if (stacks.Count > MAX_ARMY_SIZE)
+            {
+                throw new ArgumentOutOfRangeException(nameof(stacks), "Maximum size of army is 6");
+            }
+            
             _stacks = stacks;
         }
 
--- HOMM/UnitsStack.cs
+++ HOMM/UnitsStack.cs
@@ -1,12 +1,21 @@
+using System;
+
 namespace HOMM
 {
     public class UnitsStack
     {
+        public static readonly uint MAX_STACK_SIZE = 999_999;
+        
         private readonly Unit _unit;
         private readonly uint _amount;
 
         public UnitsStack(Unit unit, uint amount)
         {
+            if (amount > MAX_STACK_SIZE)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Maximum size of stack is 999 999");
+            }
+            
             _unit = unit;
             _amount = amount;
         }
